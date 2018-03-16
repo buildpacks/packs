9f34a0572ffd464b77bba34388fc3d1ff9097207
Joe Kutner <jpkutner@gmail.com>
2018-03-16 14:06:57 -0500

    Added Heroku pack implementation

---
+++ heroku/shell/shell.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"syscall"
+
+	herokuapp "github.com/sclevine/packs/heroku/app"
+)
+
+const (
+	CodeFailedEnv = iota + 1
+	CodeFailedSetup
+	CodeFailedShell
+)
+
+func main() {
+	err := os.Chdir("/home/heroku/app")
+	check(err, CodeFailedSetup, "change directory")
+
+	app, err := herokuapp.New()
+	check(err, CodeFailedEnv, "build app env")
+	for k, v := range app.Launch() {
+		err := os.Setenv(k, v)
+		check(err, CodeFailedEnv, "set app env")
+	}
+
+	args := append([]string{"/lifecycle/shell", "/home/heroku/app"}, os.Args[1:]...)
+	err = syscall.Exec("/lifecycle/shell", args, os.Environ())
+	check(err, CodeFailedShell, "run")
+}
+
+func check(err error, code int, action ...string) {
+	if err == nil {
+		return
+	}
+	message := "failed to " + strings.Join(action, " ")
+	fmt.Fprintf(os.Stderr, "Error: %s: %s", message, err)
+	os.Exit(code)
+}
