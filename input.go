a2f836c05c263393ab34e64fcd1e4dd8016cef41
Stephen Levine <stephen.levine@gmail.com>
2018-06-03 23:49:49 -0400

    Adjust metadata env var for v3 API

--- input.go
+++ input.go
@@ -6,16 +6,21 @@ import (
 )
 
 const (
-	EnvAppDir    = "PACK_APP_DIR"
-	EnvAppZip    = "PACK_APP_ZIP"
-	EnvAppName   = "PACK_APP_NAME"
-	EnvAppURI    = "PACK_APP_URI"
+	EnvAppDir = "PACK_APP_DIR"
+	EnvAppZip = "PACK_APP_ZIP"
+
+	EnvAppName = "PACK_APP_NAME"
+	EnvAppURI  = "PACK_APP_URI"
+
 	EnvAppDisk   = "PACK_APP_DISK"
 	EnvAppMemory = "PACK_APP_MEM"
 	EnvAppFds    = "PACK_APP_FDS"
 
 	EnvDropletPath  = "PACK_DROPLET_PATH"
-	EnvMetadataPath = "PACK_DROPLET_METADATA_PATH"
+	EnvSlugPath     = "PACK_SLUG_PATH"
+	EnvMetadataPath = "PACK_METADATA_PATH"
+	EnvGroupPath    = "PACK_GROUP_PATH"
+
 	EnvStackName    = "PACK_STACK_NAME"
 	EnvUseDaemon    = "PACK_USE_DAEMON"
 )
@@ -24,8 +29,16 @@ func InputDropletPath(path *string) {
 	flag.StringVar(path, "droplet", os.Getenv(EnvDropletPath), "file containing compressed droplet")
 }
 
+func InputSlugPath(path *string) {
+	flag.StringVar(path, "slug", os.Getenv(EnvSlugPath), "file containing compressed slug")
+}
+
 func InputMetadataPath(path *string) {
-	flag.StringVar(path, "metadata", os.Getenv(EnvMetadataPath), "file containing droplet metadata")
+	flag.StringVar(path, "metadata", os.Getenv(EnvMetadataPath), "file containing build metadata")
+}
+
+func InputGroupPath(path *string) {
+	flag.StringVar(path, "group", os.Getenv(EnvGroupPath), "file containing buildpack group")
 }
 
 func InputStackName(name *string) {
